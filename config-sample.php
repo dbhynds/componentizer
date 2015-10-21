af8c6833e1f7340acfdef25bf18e560d0609ffd6
dbhynds <davo@mightybytes.com>
2015-10-21 10:37:08 -0500

    add template parser path

--- config-sample.php
+++ config-sample.php
@@ -9,6 +9,10 @@
 // Path to component directory relative to current theme directory
 $component_path = 'components';
 
+// Path to HTML component templates directory (relative to current theme directory)
+// Set this to false if you do not wish to use the HTML template parser
+$html_template_path = 'html_templates';
+
 // Fields that aren't from ACF but should still be included.
 $persistant_fields = array('content');
 

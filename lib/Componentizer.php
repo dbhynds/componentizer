95ed47ecdb43e88d6db84bf4646469c59932d688
dbhynds <davo@mightybytes.com>
2016-08-12 10:30:48 -0500

    Fix name typos

--- lib/Componentizer.php
+++ lib/Componentizer.php
@@ -7,9 +7,9 @@ class Componentizer {
   function __construct()
   {
     if (is_admin()) {
-      new Componentizer\Admin\Admin;
-      new Componentizer\Admin\EditorPage;
-      new Componentizer\Admin\SettingsPage;
+      new \Componentizer\Admin\Admin;
+      new \Componentizer\Admin\EditorPage;
+      new \Componentizer\Admin\SettingsPage;
     }
   }
 
